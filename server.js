0c878254034d212535eebcdb65429061be82a45d
lararelaven <47090433+lararelaven@users.noreply.github.com>
2025-05-08 20:36:33 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const { Configuration, OpenAIApi } = require('openai');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname)));
